c33e9b2c97b9a71641585bed83917149e1f02198
John Fisher <fisherjk97@gmail.com>
2021-01-28 21:08:26 -0700

    updated unit tests

--- hello/test/problems.test.js
+++ hello/test/problems.test.js
@@ -1,8 +1,18 @@
-const Mocha = require('mocha');
+const _ = require('lodash');
+const TEST_MODE = true;
+
+/**
+ *
+ * UNIT TESTS - DO NOT TOUCH
+ *
+ */
+if (TEST_MODE) {
+  const Mocha = require('mocha');
   const mocha = new Mocha();
   const assert = require('assert');
+  mocha.suite.emit('pre-require', this, 'solution', mocha);
 
-  const p = require('../problems/interview');
+  const p = require('../problems/interview')
 
   describe('tests', () => {
     it('should reverseArrayInPlace(): empty array', () => {
@@ -12,7 +22,48 @@ const Mocha = require('mocha');
       assert.deepEqual(startingArray, reversedArray, 'returns expected output');
     });
 
-    
-});
+    it('should reverseArrayInPlace(): array with one item', () => {
+      let startingArray = _.range(1);
+      const reversedArray = startingArray.slice().reverse();
+      p.reverseArrayInPlace(startingArray);
+      assert.deepEqual(startingArray, reversedArray, 'returns expected output');
+    });
 
-mocha.run();
+    it('should reverseArrayInPlace(): array with two items', () => {
+      let startingArray = _.range(2);
+      const reversedArray = startingArray.slice().reverse();
+      p.reverseArrayInPlace(startingArray);
+      assert.deepEqual(startingArray, reversedArray, 'returns expected output');
+    });
+
+    it('should reverseArrayInPlace(): large array (even length)', () => {
+      let startingArray = _.range(100);
+      const reversedArray = startingArray.slice().reverse();
+      p.reverseArrayInPlace(startingArray);
+      assert.deepEqual(startingArray, reversedArray, 'returns expected output');
+    });
+
+    it('should reverseArrayInPlace(): large array (odd length)', () => {
+      let startingArray = _.range(101);
+      const reversedArray = startingArray.slice().reverse();
+      p.reverseArrayInPlace(startingArray);
+      assert.deepEqual(startingArray, reversedArray, 'returns expected output');
+    });
+
+    it('should reverseArrayInPlace(): array with multiple types', () => {
+      let startingArray = [10, 5, 'Danny', null, undefined, 4.5, true, new Number(5)];
+      const reversedArray = startingArray.slice().reverse();
+      p.reverseArrayInPlace(startingArray);
+      assert.deepEqual(startingArray, reversedArray, 'returns expected output');
+    });
+
+    it('should reverseArrayInPlace(): palindrome', () => {
+      let startingArray = [1, 2, 3, 3, 2, 1];
+      const reversedArray = startingArray.slice().reverse();
+      p.reverseArrayInPlace(startingArray);
+      assert.deepEqual(startingArray, reversedArray, 'returns expected output');
+    });
+  });
+
+  mocha.run();
+}
